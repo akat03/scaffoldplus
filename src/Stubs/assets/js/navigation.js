9335106d77898599372c68181d4d0f13feabf7c8
akat03 <kato@econosys-system.com>
2020-02-05 19:38:20 +0900

    [fix] delete old files

--- src/Stubs/assets/js/navigation.js
+++
@@ -1,7 +0,0 @@
-    $(document).ready(function() {
-        $('.leftmenutrigger').on('click', function(e) {
-            $('.side-nav').toggleClass("open");
-            $('#wrapper').toggleClass("open");
-            e.preventDefault();
-        });
-    });
